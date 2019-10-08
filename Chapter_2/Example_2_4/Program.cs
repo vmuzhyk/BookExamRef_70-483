7f00d05c5a872e6e3e8e074aa1e313c928fcf149
Vitaliy Muzhyk <vmuzhyk@outlook.com>
2019-10-08 23:22:59 +0300

    Create dotnet core app

---
+++ Chapter_2/Example_2_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
