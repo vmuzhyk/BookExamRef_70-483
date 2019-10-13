f3ec174be7c959bed7eae743b733e9e159086a45
Vitaliy Muzhyk <vmuzhyk@outlook.com>
2019-10-13 22:37:00 +0300

    Add lists 2_25 and 2_26. Describes of dynamic types

---
+++ Chapter_2/List_2_25/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Dynamic;
+
+namespace List_2_25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic d = 99;
+            d = d + 1;
+            Console.WriteLine(d);
+            d = "Hello";
+            d = d + " Rob";
+            Console.WriteLine(d);
+            dynamic person = new ExpandoObject();
+            person.Name = "Rob Miles";
+            person.Age = 21;
+            Console.WriteLine("Name: {0} Age: {1}", person.Name, person.Age);
+        }
+    }
+}
---
+++ Chapter_2/List_2_26_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace List_2_26_2
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                // Create the interop
+                var excelApp = new Application();
+                // make the app visible
+                excelApp.Visible = true;
+                // Add a new workbook
+                excelApp.Workbooks.Add();
+                // Obtain the active sheet from the app
+                // There is no need to cast this dynamic type
+                Worksheet workSheet = excelApp.ActiveSheet;
+                // Write into two cells
+                workSheet.Cells[1, "A"] = "Hello";
+                workSheet.Cells[1, "B"] = "from C#";
+                workSheet.Cells[2, "A"] = "Hello";
+                workSheet.Cells[2, "B"] = "Dima";
+        }
+
+        }
+}
+
+
