605909914ef000d87a42d9dfe5bc064fdb2a37db
Vitaliy Muzhyk <vmuzhyk@outlook.com>
2019-10-16 22:52:28 +0300

    Access modifiers: public, private, internal, protected

---
+++ Chapter_2/List_2_28/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_2_28
+{
+    class Customer
+    {
+        private string _nameValue;
+        public string Name
+        {
+            get
+            {
+                return _nameValue;
+            }
+            set
+            {
+                if (value == "")
+                    throw new Exception("Invalid customer name");
+                _nameValue = value;
+            }
+        }
+    }
+    class Program
+    {
+        public static void Main (string []arg)
+        {
+            Customer c = new Customer();
+            c.Name = "Rob";
+            Console.WriteLine("Customer name: {0}", c.Name);
+            Console.ReadKey();
+            // the following statement will throw an exception
+            c.Name = "";
+        }
+    }
+}
---
+++ Chapter_2/List_2_29/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_2_29
+{
+    class BankAccount
+    {
+        protected decimal accountBalance = 0;
+        public void PayInFunds(decimal amountToPayIn)
+        {
+            accountBalance = accountBalance + amountToPayIn;
+        }
+        public virtual bool WithdrawFunds(decimal amountToWithdraw)
+        {
+            if (amountToWithdraw > accountBalance)
+                return false;
+            accountBalance = accountBalance - amountToWithdraw;
+            return true;
+        }
+        public decimal GetBalance()
+        {
+            return accountBalance;
+        }
+    }
+    class OverdraftAccount : BankAccount
+    {
+        decimal overdraftLimit = 100;
+        public override bool WithdrawFunds(decimal amountToWithdraw)
+        {
+            if (amountToWithdraw > accountBalance + overdraftLimit)
+                return false;
+            accountBalance = accountBalance - amountToWithdraw;
+            return true;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount a = new BankAccount();
+            a.PayInFunds(50);
+            Console.WriteLine("Pay in 50");
+            a.PayInFunds(50);
+            if (a.WithdrawFunds(10))
+                Console.WriteLine("Withdrawn 10");
+            Console.WriteLine("Account balance is: {0}", a.GetBalance());
+            Console.ReadKey();
+        }
+    }
+}
