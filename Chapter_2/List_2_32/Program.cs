79f0fa1805c7ed74f0e117180b25f5a804074098
Vitaliy Muzhyk <vmuzhyk@outlook.com>
2019-10-19 23:09:03 +0300

    Interfaces

---
+++ Chapter_2/List_2_32/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_2_32
+{
+    interface IPrintable
+    {
+        string GetPrintableText(int pageWidth, int pageHeight);
+        string GetTitle();
+    }
+    interface IDisplay
+    {
+        string GetTitle();
+    }
+    class Report : IPrintable, IDisplay
+    {
+        string IPrintable.GetPrintableText(int pageWidth, int pageHeight)
+        {
+            return "Report text to be printed";
+        }
+        string IPrintable.GetTitle()
+        {
+            return "Report title to be printed";
+        }
+        string IDisplay.GetTitle()
+        {
+            return "Report title to be displayed";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
