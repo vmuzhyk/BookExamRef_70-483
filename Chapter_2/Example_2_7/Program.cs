f827c8d7d4242812a83127eb9a6d68a4cc53e1cc
Vitaliy Muzhyk <vmuzhyk@outlook.com>
2019-11-03 18:33:48 +0200

    List_2_45, Idisposable

---
+++ Chapter_2/Example_2_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int index = rand.Next(1, 185);
+            Console.WriteLine(index);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter_2/List_2_45/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_2_45
+{
+    class CrucialConnection : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose called");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (CrucialConnection c = new CrucialConnection())
+            {
+                // do something with the crucial connection
+            }
+            Console.ReadKey();
+        }
+    }
+}
