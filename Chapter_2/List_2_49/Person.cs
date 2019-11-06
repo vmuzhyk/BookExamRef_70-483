09b7e0ef67ad903e5ed1be306b7d92f3ae44d51e
Vitaliy Muzhyk <vmuzhyk@outlook.com>
2019-11-06 21:35:52 +0200

    Atributes

---
+++ Chapter_2/List_2_49/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_2_49
+{
+    [Programmer("Fred")]
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Chapter_2/List_2_49/ProgrammerAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_2_49
+{
+    class ProgrammerAttribute : Attribute
+    {
+        private string programmerValue;
+        public ProgrammerAttribute(string programmer)
+        {
+            programmerValue = programmer;
+        }
+        public string Programmer
+        {
+            get
+            {
+                return programmerValue;
+            }
+        }
+    }
+}
