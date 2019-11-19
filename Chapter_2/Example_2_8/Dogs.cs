c5114b5abd68f25c10525e570be97882b5be51a5
Vitaliy Muzhyk <vmuzhyk@outlook.com>
2019-11-19 23:01:38 +0200

    Ensure that structure can contain private members

---
+++ Chapter_2/Example_2_8/Dogs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2_8
+{
+    struct Dogs
+    {
+        private int teeth;
+        private int Age { get; set; }
+        private string Gav()
+        {
+            return "woof";
+        }
+    }
+}
